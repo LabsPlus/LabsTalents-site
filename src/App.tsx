ad5df2add5c983307256b2d1fb01405ab662f067
marleypm16 <yarlempm16@gmail.com>
2024-10-03 09:36:46 -0300

    feat : login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className='container'>
+        <div className='left-container'>
+          <img src='./assets/labsTalentsImage.png' alt="labsTalentsImage" />
+        </div>
+        <div className='right-container'>
+          <div className='form-container'>
+            <h1 className='title'>Bem vindo ao LabsTalents</h1>
+            <div className='register_link_container'>
+            <p className='text'>Novo aqui?</p> <span><a href='/' className='text destaque'>Crie uma conta</a></span>
+            </div>
+            <form action="">
+              <div className='inputs-container'>
+                <input className='input' type="text" name="username" id="username" placeholder='Usuario*' />
+                <input className='input' type="password" name="password" id="password" placeholder='Senha*' />
+              </div>
+              <div className='remember-device-register-link'>
+              <div className='remember-device'>
+                  <input type="checkbox" name="" id="" />
+                  <span className='text'>Lembre deste dispositivo</span> 
+              </div>
+                <span><a href='/' className='text destaque'>Crie uma conta</a></span>
+              </div>
+
+              <button type='submit'>
+                Logar
+              </button>
+            </form>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
