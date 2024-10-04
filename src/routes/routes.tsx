0791e81283b658eee0b919fe2973369122ea4850
marleypm16 <yarlempm16@gmail.com>
2024-10-04 11:07:47 -0300

    feat : add forget password route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Login from "../pages/login/Login";
+import ForgetPassword from "../pages/forget-password/forgetPassword";
 
 const Routers = ()=>{
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login/>}></Route>
+                <Route path="/forget-password" element={<ForgetPassword/>}></Route>
             </Routes>
         
         
