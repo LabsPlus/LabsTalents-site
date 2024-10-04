ea4f13b8cc1a9e6c656186e2d7537770e6dfa719
marleypm16 <yarlempm16@gmail.com>
2024-10-04 11:08:10 -0300

    feat : add forget password layout

--- src/pages/forget-password/forgetPassword.tsx
+++ src/pages/forget-password/forgetPassword.tsx
@@ -1,7 +1,37 @@
-const forgetPassword = () =>{
+import Input from "../../components/input/input"
+import LabsTalentImage from "../../assets/labsTalentsImage.png"
+import './style.css'
+const ForgetPassword = () =>{
     return(
-        <h1>Esqueci a senha</h1>
+    <main className='container'>
+        <div className='left-container'>
+            <img src={LabsTalentImage} alt="labsTalentsImage" />
+        </div>
+        <div className='right-container'>
+            <div className='form-container'>
+                <div className="ajustar-largura">
+                    <h1 className='title'>Esqueceu sua senha?</h1>
+                    <p className='text'>Por favor, insira o endereço de e-mail <br /> associado à sua conta e enviaremos um e-mail <br /> com um link para redefinir sua senha.</p>
+                    <form action="">
+                        <div className='inputs-container'>
+                            <Input type='email' placeholder='Email*'/>
+                        </div>
+                        <button type='submit'>
+                            Esqueci a senha
+                        </button>
+                        <button className="back-login-link">
+                            <a href="/login">
+                                Retornar ao Login
+                            </a>
+                        </button>
+
+                    </form>
+                </div>
+                
+            </div>
+        </div>
+    </main>
     )
 }
 
-export default forgetPassword
+export default ForgetPassword
