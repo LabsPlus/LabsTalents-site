8c9bd1a650f17873efadbc254d8a9235a53b3b64
marleypm16 <yarlempm16@gmail.com>
2024-10-07 10:27:53 -0300

    fix conflict

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -22,9 +22,8 @@ const Login = () =>{
                     <div className='remember-device'>
                         <input type="checkbox" name="" id="" />
                         <span className='text'>Lembre deste dispositivo</span> 
-                    </div>                    
-
-                        <span><a href='/' className='text destaque'>Esqueceu a senha?</a></span>
+                    </div> 
+                    <span><a href='/' className='text destaque'>Esqueceu a senha?</a></span>
                     </div>
 
                     <button type='submit'>
