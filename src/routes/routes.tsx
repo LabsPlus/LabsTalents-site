bb8f1e08d0dce496419240f7d4fe6329f15dc200
marleypm16 <yarlempm16@gmail.com>
2024-10-07 10:08:10 -0300

    feat : create new password page route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Login from "../pages/login/Login";
 import ForgetPassword from "../pages/forget-password/forgetPassword";
+import NewPassword from "../pages/new-password/NewPassword";
 
 const Routers = ()=>{
     return(
@@ -8,6 +9,7 @@ const Routers = ()=>{
             <Routes>
                 <Route path="/login" element={<Login/>}></Route>
                 <Route path="/forget-password" element={<ForgetPassword/>}></Route>
+                <Route path="/new-password" element={<NewPassword/>}></Route>
             </Routes>
         
         
