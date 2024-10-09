7e25547703871c6f5c6703c3a78a9cf129906a15
marleypm16 <yarlempm16@gmail.com>
2024-10-09 10:50:29 -0300

    feat : add logo

--- src/layout/auth-register-user.tsx
+++ src/layout/auth-register-user.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
 import LabsTalentImage from '../assets/labsTalentsImage.png'
+import LabsTalentLogo from '../assets/Logo.png'
 const AuthRegisterUser = () =>{
     return(
         <main className='container'>
                 <div className='left-container'>
+                <img className="logo" src={LabsTalentLogo} alt="labs talent logo" />
                 <img src={LabsTalentImage} alt="labsTalentsImage" />
                 </div>
                 <div className='right-container'>
