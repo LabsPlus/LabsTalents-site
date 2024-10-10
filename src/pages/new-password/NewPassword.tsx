38916d82bcd4d8678a94cc15610c26f322bf5fba
marleypm16 <yarlempm16@gmail.com>
2024-10-10 10:16:32 -0300

    fixing styles

--- src/pages/new-password/NewPassword.tsx
+++ src/pages/new-password/NewPassword.tsx
@@ -4,16 +4,17 @@ import { FormContainer } from '../../styles/formContainer'
 import { Title } from '../../styles/title'
 import { BackLoginLink } from '../../styles/links'
 import { Form } from '../login/loginStyles'
+import { InputContainer } from '../../styles/inputContainer'
 const NewPassword = () =>{
     return(
         <FormContainer >
             <div className="ajustar-largura">
                 <Title>Digite a nova senha:</Title>
                 <Form action="">
-                    <div>
+                    <InputContainer>
                         <Input type='password' placeholder='Senha*'/>
                         <Input type='password' placeholder='Confirma a senha*'/>
-                    </div>
+                    </InputContainer>
                     <Button type='submit'>
                         Registrar nova senha
                     </Button>        
--- src/styles/title.ts
+++ src/styles/title.ts
@@ -4,4 +4,5 @@ export const Title = styled.h1`
     font-size: 24px;
     font-weight: 500;
     color: #404857;
+    margin-bottom:15px
 `
